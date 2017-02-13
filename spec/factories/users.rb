e404b6b3946d719e54fcdeb70a17f7fb98b4431f
Anna Dolan <anna.dolan@gmail.com>
2017-02-13 14:30:44 -0700

    Links index shows only current user's links

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :user do
     name "MyString"
-    email "MyString"
+    sequence :email do |n|
+       "user#{n}@example.com"
+     end
     password "MyString"
   end
 end
