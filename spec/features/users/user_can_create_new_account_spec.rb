1010e095a60f78de97c8e618d2fbef1a9bbb9f0a
Anna Dolan <anna.dolan@gmail.com>
2017-02-13 13:06:27 -0700

    New user sad path

--- spec/features/users/user_can_create_new_account_spec.rb
+++ spec/features/users/user_can_create_new_account_spec.rb
@@ -13,4 +13,15 @@ describe "A user visits the signup form" do
     expect(current_path).to eq(root_path)
     expect(page).to have_content("Welcome Anna!")
   end
+
+  it "And leaves a field blank" do
+    visit signup_path
+
+    fill_in "Name", with: "Anna"
+    fill_in "Email", with: "anna@anna.com"
+
+    click_on "Sign Up"
+
+    expect(page).to have_content("There was a problem creating your account.")
+  end
 end
